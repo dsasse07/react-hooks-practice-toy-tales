66b8f60d2918c98b7b43ccf2a1abd28986da1e2f
Daniel Sasse <dsasse07@gmail.com>
2021-02-26 18:17:42 -0500

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
+  const API = "http://localhost:3001/toys"
   const [showForm, setShowForm] = useState(false);
+  const [toyData, setToyData] = useState([])
+
+  useEffect ( () => {
+    fetch(API)
+      .then( response => response.json() )
+      .then( setToyData )
+
+  }, [])
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function handleFormSubmit(formData){
+    const postConfig = {
+      method:"POST",
+      headers:{
+        "Content-type":"application/json"
+      },
+      body: JSON.stringify( formData )
+    }
+    fetch(API, postConfig)
+      .then( response => response.json() )
+      .then( newToyData => {
+        setToyData([...toyData, newToyData])
+      })
+  }
+
+  function handleDeleteToy(id){
+    fetch(`${API}/${id}`, {method:"DELETE"} )
+    const newToyData = toyData.filter( toy => {
+      return toy.id !== id
+    })
+    setToyData(newToyData)
+  }
+
+  function handleLikeToy({id, likes}){
+    const patchConfig = {
+      method:"PATCH",
+      headers:{
+        "Content-type":"application/json"
+      },
+      body: JSON.stringify( {likes: parseInt(likes) + 1 })
+    }
+
+    fetch(`${API}/${id}`, patchConfig )
+      .then( response => response.json() )
+      .then( updatedToy => {
+        const newToysArray = toyData.map( toy => {
+          if (toy.id !== updatedToy.id) return toy
+          return updatedToy
+        })
+        setToyData([...newToysArray])
+      })
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onFormSubmit={handleFormSubmit}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toyData={toyData} onToyDelete={handleDeleteToy} onLikeToy={handleLikeToy}/>
     </>
   );
 }
