c5cd8d55678578baea4f039831dbabb9ac948f18
Daniel Sasse <dsasse07@gmail.com>
2021-02-26 18:17:38 -0500

    commit tree 768faf264ca200b87268691959f32e6e27c89835

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,17 +1,26 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({ onLikeToy, onToyDelete, toy:{id, name, image, likes} }) {
+
+  function handleDeleteToy(){
+    onToyDelete(id)
+  }
+
+  function handleLikeToy(){
+    onLikeToy({id, likes})
+  }
+  
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={image}
+        alt={name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{likes} Likes </p>
+      <button className="like-btn" onClick={handleLikeToy}>Like {"<3"}</button>
+      <button className="del-btn" onClick={handleDeleteToy}>Donate to GoodWill</button>
     </div>
   );
 }
